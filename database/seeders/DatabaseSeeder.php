03c892b9516456ff2182469813af831258ccbdf6
BhaveshChandpa <Bhaveshchandpa33@gmail.com>
2024-06-17 21:37:18 +0530

    Product Seeder Pending

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([TaxSeeder::class]);
 
         $this->call([CategorySeeder::class]);
     }
--- database/seeders/TaxSeeder.php
+++ database/seeders/TaxSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tax;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class ProductSeeder extends Seeder
+class TaxSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Tax::factory(3)->create();
     }
 }
