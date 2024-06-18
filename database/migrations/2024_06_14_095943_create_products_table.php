b50c4f16771d8788e0d97353e9c6a857558e585c
Bhavesh Chandpa <bhaveshchandpa33@example.com>
2024-06-18 14:26:34 +0530

    Seeded

--- database/migrations/2024_06_14_095943_create_products_table.php
+++ database/migrations/2024_06_14_095943_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->foreignIdFor(Category::class);
             $table->unsignedBigInteger('subcategory_id')->constrained('categories');
             $table->double('price');
