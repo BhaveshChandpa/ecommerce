ad6719903880a2a1c968caab25fd91dc21aefbf1
Bhavesh Chandpa <bhaveshchandpa33@example.com>
2024-07-10 14:04:15 +0530

    notification

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Notifications\SuccessLogin;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
@@ -45,6 +46,8 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+
+        $user->notify(new SuccessLogin($user->user));
 
         return redirect(RouteServiceProvider::HOME);
     }
